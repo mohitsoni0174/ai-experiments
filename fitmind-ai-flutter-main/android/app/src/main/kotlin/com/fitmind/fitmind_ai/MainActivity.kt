40a233b8d03277da35522edbfe736399ce3238bc
Mohit Soni <mohitsoni1704@gmail.com>
2026-06-14 09:04:36 +0530

    Add AI experiments

--- fitmind-ai-flutter-main/android/app/src/main/kotlin/com/fitmind/fitmind_ai/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.fitmind.fitmind_ai
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- fitmind-ai-flutter-main/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'package:fitmind_ai/app.dart';
-
-void main() {
-  runApp(const FitMindApp());
-}
